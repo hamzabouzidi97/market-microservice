28e614642ff8c650c2e54a4b5acf0809695a7205
hamzabouzidi97 <bouzidi.hamza.pro@gmail.com>
2022-12-25 00:34:23 +0100

    add reselience4j properties and fix dependency

--- order-service/src/main/java/com/market/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/market/orderservice/controller/OrderController.java
@@ -4,9 +4,13 @@ package com.market.orderservice.controller;
 import com.market.orderservice.dto.OrderRequest;
 import com.market.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -18,12 +22,14 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order placed successfully";
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
     }
 
-    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
-        return "Oops! something went wrong, please try again after some time!";
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()-> "Oops! something went wrong, please try again after some time!") ;
     }
+
 }
